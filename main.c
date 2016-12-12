6847be14527df9a378a00c68344a5d3123691380
arakhsha <a.rakhsha2016@student.sharif.edu>
2016-12-12 17:34:49 +0330

    adding isPrime function to main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int isPrime(int n)
+{
+	if(n <= 1)
+		return 0;
+	int i = 2;
+	for(i = 2; i * i <= n; i++)
+		if(n % i == 0)
+			return 0;
+	return 1;
+}
+
 void main()
 {
 	int a, b;
