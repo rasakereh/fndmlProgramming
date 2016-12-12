91b271848ec59ea5505b8e82fbfb9550e015d462
rasakereh <rasakereh@ce.sharif.edu>
2016-12-12 16:56:23 +0330

    New commit is here multi line test

--- main.c
+++ main.c
@@ -2,5 +2,9 @@
 
 void main()
 {
+	int a, b;
 	printf("Hello World!");
+	printf("\n Enter two nums:\t");
+	scanf("%d%d", &a, &b);
+	printf("\n%d + %d = %d\n", a, b, a+b);
 }
---
+++ newGit.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("I am new to git :D hurraaaaaaaaaaay!");
+}
